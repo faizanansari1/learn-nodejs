9cada4b50a012c6f832cbb8d27c44e324e72fa6c
faizan-bp <faizan@bitspro.com>
2022-08-04 15:52:38 +0500

    repl description in index file

--- index.js
+++ index.js
@@ -1,34 +1,4 @@
-// Event and EventEmitter
-// EventEmitter simply like a button but in node js has no create any button.
-// in node js you have to create himsilf event and also use in the API Like Blow in the cods
-
-const express = require("express");
-const EventEmitter = require("events");
-const app = express();
-
-let count = 0;
-
-const event = new EventEmitter();
-
-event.on("countApi", () => {
-  count++;
-  console.log("Event Called" + count);
-});
-
-app.get("/", (req, resp) => {
-  resp.send("Api Called");
-  // create event for hit the api
-  event.emit("countApi");
-});
-
-app.get("/search", (req, resp) => {
-  resp.send("search Called");
-  event.emit("countApi");
-});
-
-app.get("/update", (req, resp) => {
-  resp.send("update Called");
-  event.emit("countApi");
-});
-
-app.listen(5000);
+// REPL
+// Read-Eval-Print-Loop ==== Eval > (Evalutor)
+// ap repl ky upper javascript ka code bhi or nodejs ka nc
+// when you open the cmd then inter node press enter then you can in the evalutor of node js
