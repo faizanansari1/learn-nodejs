aa1dd7649e87ce48c539b54c57c7f153c6d83577
faizan-bp <faizan@bitspro.com>
2022-08-04 15:53:50 +0500

    repl description in index files

--- index.js
+++ index.js
@@ -2,3 +2,5 @@
 // Read-Eval-Print-Loop ==== Eval > (Evalutor)
 // ap repl ky upper javascript ka code bhi or nodejs ka nc
 // when you open the cmd then inter node press enter then you can in the evalutor of node js
+//.edior
+//.help
