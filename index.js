c091858fdf555de2bb210f6357911cc62cecaeba
faizan-bp <faizan@bitspro.com>
2022-08-04 12:53:31 +0500

    file upload

--- index.js
+++ index.js
@@ -1,45 +1,24 @@
+// file upload using api .. we need to install multer packege for uploading the file
 const express = require("express");
-require("./config");
-const products = require("./product");
+const { now } = require("mongoose");
+const multer = require("multer");
 const app = express();
 app.use(express.json());
 
-app.post("/create", async (req, resp) => {
-  let data = new products(req.body);
-  // save on dataBase
-  let result = await data.save();
-  console.log("REQ", result);
-  resp.send(result);
-});
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "uploads");
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + "-" + Date.now() + ".jpg");
+    },
+  }),
+}).single("user_file");
 
-app.get("/list", async (req, resp) => {
-  let data = await products.find();
-  resp.send(data);
-});
-
-app.delete("/delete/:_id", async (req, resp) => {
-  console.log(req.params);
-  let data = await products.deleteOne(req.params);
-  resp.send(data);
-});
-
-app.put("/update/:_id", async (req, resp) => {
-  // console.warn(req.params);
-  let data = await products.updateOne(req.params, { $set: req.body });
-  resp.send(data);
-});
-
-// for searching
-
-app.get("/search/:key", async (req, resp) => {
-  let data = await products.find({
-    $or: [
-      { name: { $regex: req.params.key } },
-      { brand: { $regex: req.params.key } },
-    ],
-  });
-
-  resp.send(data);
+app.post("/upload", upload, (req, resp) => {
+  console.log("req>>", req.body);
+  resp.send("FILE Uploaded jkahds");
 });
 
 app.listen(5000);
